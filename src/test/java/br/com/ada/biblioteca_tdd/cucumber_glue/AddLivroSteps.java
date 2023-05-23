46f9bfba44dc7b29c1fae92d7a83cfed21b0320d
Bruno Ferreira <brunontf@gmail.com>
2023-05-23 17:17:42 -0300

    Update AddLivroSteps.java

--- src/test/java/br/com/ada/biblioteca_tdd/cucumber_glue/AddLivroSteps.java
+++ src/test/java/br/com/ada/biblioteca_tdd/cucumber_glue/AddLivroSteps.java
@@ -25,7 +25,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import java.time.LocalDate;
 
-@CucumberContextConfiguration
+//@CucumberContextConfiguration
 @SpringBootTest
 @AutoConfigureMockMvc
 
